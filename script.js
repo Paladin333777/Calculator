7d749425cc53564a81b0fdbb05236a80b962979d
Paladin333777 <romavolvyn@gmail.com>
2023-07-05 22:43:08 -0400

    Add all files, and basic HTML with CSS.

---
+++ script.js
@@ -0,0 +1,12 @@
+const add = function(num1, num2){return num1 + num2};
+const subtract = function(num1, num2){return num1 - num2};
+const multiply = function(num1, num2){return num1 * num2};
+const divide = function(num1, num2){return num1 / num2};
+
+let firstVar
+let secondVar
+let thirdVar
+
+
+
+console.log(divide(2, 5))
